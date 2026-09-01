54ba87daa2b535e1b4bba3c85a14d82ee726a672
Smith Parekh <smithparekh107@gmail.com>
2026-09-01 14:59:42 +0530

    Add enriched conversion intermediate model

---
+++ affiliate_analytics/models/intermediate/int_conversions_enriched.sql
@@ -0,0 +1,40 @@
+{{ config(
+    materialized='table'
+) }}
+
+select
+    f.conversion_id,
+    f.click_id,
+    c.customer_key,
+    f.user_id,
+    f.order_id,
+    f.converted_at,
+
+    p.product_key,
+    p.source_asin,
+    p.product_title,
+    p.brand,
+    p.category,
+    p.subcategory,
+
+    f.order_value,
+    f.commission_rate,
+    f.commission_earned,
+    f.quantity_purchased,
+    f.conversion_time_hours,
+    f.customer_type,
+    f.payment_method,
+    f.shipping_method,
+    f.order_status,
+    f.return_status,
+    f.customer_lifetime_value,
+    f.previous_orders_count
+
+from {{ ref('fct_conversions') }} f
+
+left join {{ ref('dim_customer') }} c
+    on f.user_id = c.user_id
+
+left join {{ ref('dim_product') }} p
+    on f.source_asin = p.source_asin
+   and f.product_title = p.product_title
